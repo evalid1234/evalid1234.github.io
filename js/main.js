f7c6808146148f29e38defa3c74bba411eb6b4a8
ed lagu <elagu013@fiu.edu>
2023-11-08 14:50:18 -0500

    added mobile menu

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 //loader
 document.addEventListener("DOMContentLoaded", function (event) {
   document.querySelector(".loading_group").style.display = "none";
-  
 });
 
 //home fade in
 window.addEventListener("load", function () {
   const home = document.querySelector("#home");
-   home.classList.add("show");
+  home.classList.add("show");
 });
 
 /* fade in */
@@ -111,67 +110,67 @@ observer.observe(document.querySelector("#typing-text"));
 
 /* FORMS */
 
-const form = document.querySelector("form");
-const feedback = document.querySelector("#feedback");
+// const form = document.querySelector("form");
+// const feedback = document.querySelector("#feedback");
 
-const messageInput = document.getElementById("message");
+// const messageInput = document.getElementById("message");
 
-messageInput.addEventListener("input", function (event) {
-  const input = event.target;
-  const inputValue = input.value;
-  const cleanedInputValue = inputValue.replace(/[^a-zA-Z0-9\s,._-]/g, "");
-  input.value = cleanedInputValue;
-});
+// messageInput.addEventListener("input", function (event) {
+//   const input = event.target;
+//   const inputValue = input.value;
+//   const cleanedInputValue = inputValue.replace(/[^a-zA-Z0-9\s,._-]/g, "");
+//   input.value = cleanedInputValue;
+// });
 
-form.addEventListener("input", function (event) {
-  const input = event.target;
+// form.addEventListener("input", function (event) {
+//   const input = event.target;
 
-  if (input.checkValidity()) {
-    input.classList.remove("invalid");
-    input.classList.add("valid");
-    feedback.classList.remove("visible");
-    feedback.classList.add("invisible");
-    feedback.textContent = "";
-  } else {
-    input.classList.remove("valid");
-    input.classList.add("invalid");
-    feedback.classList.remove("invisible");
-    feedback.classList.add("visible");
+//   if (input.checkValidity()) {
+//     input.classList.remove("invalid");
+//     input.classList.add("valid");
+//     feedback.classList.remove("visible");
+//     feedback.classList.add("invisible");
+//     feedback.textContent = "";
+//   } else {
+//     input.classList.remove("valid");
+//     input.classList.add("invalid");
+//     feedback.classList.remove("invisible");
+//     feedback.classList.add("visible");
 
-    // Provide specific feedback messages for each input field
-    switch (input.name) {
-      case "name":
-        feedback.textContent =
-          "Please enter a valid name (letters and spaces only)";
-        break;
-      case "subject":
-        feedback.textContent =
-          "Please enter a valid email header (no numbers or special characters)";
-        break;
-      case "email":
-        if (!input.value.includes("@")) {
-          feedback.textContent = 'Please include an "@" in the email address';
-        } else if (!input.value.includes(".com")) {
-          feedback.textContent =
-            "Please include a valid domain (.com, .org, etc.)";
-        } else {
-          feedback.textContent = "Please enter a valid email address";
-        }
-        break;
-      case "message":
-        if (input.value.length > 100) {
-          feedback.textContent = "Message cannot be longer than 100 characters";
-        } else {
-          feedback.textContent =
-            "Please enter a valid message (letters, numbers, spaces, dashes, and underscores only)";
-        }
-        break;
-      default:
-        feedback.textContent = "Please fill out all required fields";
-        break;
-    }
-  }
-});
+//     // Provide specific feedback messages for each input field
+//     switch (input.name) {
+//       case "name":
+//         feedback.textContent =
+//           "Please enter a valid name (letters and spaces only)";
+//         break;
+//       case "subject":
+//         feedback.textContent =
+//           "Please enter a valid email header (no numbers or special characters)";
+//         break;
+//       case "email":
+//         if (!input.value.includes("@")) {
+//           feedback.textContent = 'Please include an "@" in the email address';
+//         } else if (!input.value.includes(".com")) {
+//           feedback.textContent =
+//             "Please include a valid domain (.com, .org, etc.)";
+//         } else {
+//           feedback.textContent = "Please enter a valid email address";
+//         }
+//         break;
+//       case "message":
+//         if (input.value.length > 100) {
+//           feedback.textContent = "Message cannot be longer than 100 characters";
+//         } else {
+//           feedback.textContent =
+//             "Please enter a valid message (letters, numbers, spaces, dashes, and underscores only)";
+//         }
+//         break;
+//       default:
+//         feedback.textContent = "Please fill out all required fields";
+//         break;
+//     }
+//   }
+// });
 
 const isMobileBrowser = (window.mobileAndTabletCheck = (function () {
   let check = false;
@@ -194,7 +193,7 @@ const showToolTip = (element) => {
   const child = element.firstElementChild;
   if (child.style.visibility === "hidden") {
     child.style.visibility = "visible";
-    element.style.fontWeight = "400";
+    element.style.fontWeight = "600";
   } else {
     child.style.visibility = "hidden";
     element.style.fontWeight = "200";
@@ -209,3 +208,37 @@ if (isMobileBrowser) {
     });
   });
 }
+
+const menu_btn = document.querySelector(".menu_btn");
+const sidebar = document.querySelector(".sidebar");
+const CURRENT_MOBILE_SIZE_END = 701;
+
+menu_btn.addEventListener("click", () => {
+   
+
+  menu_btn.classList.toggle("open");
+  sidebar.classList.toggle("open");
+});
+
+menu_btn.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    menu_btn.click();
+  }
+});
+
+window.addEventListener(
+  "resize",
+  function (event) {
+    if (
+      this.window.innerWidth >= CURRENT_MOBILE_SIZE_END &&
+      sidebar.classList.contains("open")
+    ) {
+      sidebar.classList.toggle("open");
+      menu_btn.classList.toggle("open");
+    }
+  },
+  true
+);
